10bf987fd2e0fedf1056ae59e1e6b36512d3b01c
Travis Walton <travisdeswalton@gmail.com>
2018-03-20 23:11:42 -0500

    QuickSort added.

---
+++ sort-algorithm-comparison/QuickSort.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sort_algorithm_comparison
+{
+    class QuickSort
+    {
+        public QuickSort(int[] args)
+        {
+            int size = args.Length;
+            Console.WriteLine("QuickSort: Unsorted: ");
+            ShowArray(args, size);
+
+            Sort(args, size);
+
+            Console.WriteLine("QuickSort: Sorted: ");
+            ShowArray(args, size);
+        }
+
+        public static void ShowArray(int[] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+
+        }
+        public static void Sort(int[] a, int n)
+        {
+            Sort(a, 0, n - 1);
+        }
+
+        private static void Sort(int[] a, int low, int up)
+        {
+            if (low >= up) // zero or one element in sublist
+                return;
+            int p = Partition(a, low, up);
+            Sort(a, low, p - 1); // sort left sublist
+            Sort(a, p + 1, up); // sort right sublist
+        }
+        private static int Partition(int[] a, int low, int up)
+        {
+            int temp, i, j, pivot;
+
+            pivot = a[low];
+
+            i = low + 1; //moves from left to right
+            j = up; // moves from right to left
+
+            while (i <= j)
+            {
+                while (a[i] < pivot && i < up)
+                    i++;
+
+                while (a[j] > pivot)
+                    j--;
+
+                if (i < j) // swap a[i] and a[j]
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                    i++;
+                    j--;
+                }
+                else // found proper place for pivot
+                    break;
+            }
+
+            // proper place for pivot is j
+            a[low] = a[j];
+            a[j] = pivot;
+
+            return j;
+        }
+    }
+}
